66e0c2ef07481be6795220dce6ebf9538d44ff0d
Darshak Devani <devanidarshak01@gmail.com>
2020-12-15 09:53:37 +0530

    add gitignore,procfile and update ports

--- app.js
+++ app.js
@@ -123,9 +123,11 @@ app.post("/delete", function(req,res) {
   
 });
 
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
 
-
-
-app.listen(process.env.PORT || 3000, function() {
+app.listen(port, function() {
   console.log("Server started on port 3000");
 });
